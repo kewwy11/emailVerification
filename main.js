99d734686d82a71e28e68a361134c24ab1b3443f
kewwy11 <erhisohwodegratefulruki@gmail.com>
2023-02-13 21:35:12 +0100

    final commit

--- main.js
+++ main.js
@@ -5,12 +5,23 @@ const lastName = document.getElementById('lastname')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const firstnameError = document.getElementById('firstname-error')
+const lastnameError = document.getElementById('lastname-error')
+const emailError = document.getElementById('email-error')
+const passwordError = document.getElementById('password-error')
 const form = document.getElementById('form')
 const validName = /^[A-Za-z]+$/;
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    validatefirstName()
+    validateLastName()
+    validateEmail()
+    validatePassword()
+
+})
+
+function validatefirstName(){
     if(firstName.value.trim() === ''){
         firstnameError.innerHTML = 'Firstname is required'
         return false;
@@ -19,11 +30,61 @@ form.addEventListener('submit', (e) => {
         firstnameError.innerHTML = "Not a valid first name"
         return false
     }
-    else{
-        firstnameError.innerHTML = ''
+    if(firstnameError.innerHTML = ''){
         return true
     }
-})
+
+}
+
+function validateLastName(){
+    if(lastName.value.trim() === ''){
+        lastnameError.innerHTML = 'Lastname is required'
+        return false;
+    }
+    if(!validName.test(lastName.value.trim())){
+        lastnameError.innerHTML = "Not a valid last name"
+        return false
+    }
+    if(lastnameError.innerHTML = ''){
+        return true
+    }
+}
+
+function validatePassword(){
+    const validPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if(password.value.trim() === ''){
+        passwordError.innerHTML = 'Password is required'
+        return false;
+    }
+
+    if(!validPassword.test(password.value.trim())){
+        passwordError.innerHTML = 'Password must include A, a and #'
+        return false;
+    }
+    else{
+        passwordError.innerHTML = ''
+        return true
+    }
+}
+
+function validateEmail(){
+    const validEmailAddress = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if(email.value.trim() === ''){
+        emailError.innerHTML = 'Email Address is required'
+        return false;
+    }
+
+    if(!validEmailAddress.test(email.value.trim())){
+        emailError.innerHTML = 'Not a valid email address'
+        return false;
+    }
+    else{
+        emailError.innerHTML = ''
+        return true
+    }
+}
 
 inputs.forEach(input => {
     input.addEventListener('click', () => {
